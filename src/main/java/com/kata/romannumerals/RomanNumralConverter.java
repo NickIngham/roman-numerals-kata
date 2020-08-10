52b147e10777d66e5c49c9387be9b1040ed56e66
Nick Ingham <>
2020-08-10 22:37:30 +0100

    can convert 2 into II

--- src/main/java/com/kata/romannumerals/RomanNumralConverter.java
+++ src/main/java/com/kata/romannumerals/RomanNumralConverter.java
@@ -3,6 +3,8 @@ package com.kata.romannumerals;
 public class RomanNumralConverter {
 
     public String convert(int i) {
+        if (i == 2)
+            return "II";
         return "I";
     }
 }
--- src/test/java/com/kata/romannumerals/RomanNumeralConverterTest.java
+++ src/test/java/com/kata/romannumerals/RomanNumeralConverterTest.java
@@ -16,4 +16,15 @@ class RomanNumeralConverterTest {
 
         assertEquals(expected, result);
     }
+
+    @Test
+    void givenAValueOfTwoEntered_WhenConverterIsCalled_ThenIIisReturned(){
+        var number = 1;
+        var converter = new RomanNumralConverter();
+        var expected = "II";
+
+        var result = converter.convert(2);
+
+        assertEquals(expected, result);
+    }
 }
